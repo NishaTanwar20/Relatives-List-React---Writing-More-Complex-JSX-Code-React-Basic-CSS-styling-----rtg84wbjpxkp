9cd934ee3b55310e9df2812f9af2f53c8ff109e9
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-14 19:15:29 +0530

    key added to ol tag

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,17 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-     const relativeNames=[
-        {name:'nisha',key:'relativeListItem1'},
-        {name:'nidhi',key:'relativeListItem2'}
-     ];
+        const relativeList=[
+            {name:'nisha',key:'relativeListItem1'},
+            {name:'nidhi',key:'relativeListItem2'}
+        ];
        
         return(
             <div id="main">
                {/* Do not remove the main div */}
                
                 <ol key={relativeList}>
-                    {relativeNames.map(relative => {
+                    {relativeList.map(relative => {
                         return (
                             <li key={relative.key}>{relative.name}</li>
                         )
