32db8c9400d63c60b060ad2d54720ad2952e559b
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-17 18:30:02 +0530

    added key

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const relativeList=[
+    {name:'nisha',key:'relativeListItem1'},
+    {name:'nidhi',key:'relativeListItem2'}
+];
+
 class App extends Component {
+
     render() {
-        const relativeList=[
-            {name:'nisha',key:'relativeListItem1'},
-            {name:'nidhi',key:'relativeListItem2'}
-        ];
+       
        
         return(
             <div id="main">
                {/* Do not remove the main div */}
                
-                <ol key={relativeList}>
-                    {relativeList.map(relative => {
+               <ol key="relativeList">
+                    {relativeList.map((relative) => {
                         return (
                             <li key={relative.key}>{relative.name}</li>
                         )
